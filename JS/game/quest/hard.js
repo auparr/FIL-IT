b91e7bbc037d7fc18085799039c29c42130f8555
auparr <aufarobbani7@gmail.com>
2025-10-01 07:22:08 +0700

    fix script link

--- JS/game/quest/hard.js
+++ JS/game/quest/hard.js
@@ -3,7 +3,6 @@ function hardQuestionGenerator() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  // Fungsi untuk memilih elemen acak dari array
   function getRandomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
